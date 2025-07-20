d5e41c055fe71d64e41650aeab050dc6abd61724
osuplus <shuboo2103@icloud.com>
2025-07-20 21:20:24 +0900

    Create reservation.js

---
+++ reservation.js
@@ -0,0 +1,62 @@
+const allReservations = [
+  {
+    id: 1,
+    date: "2025年6月15日",
+    store: "サンプルカフェ渋谷店",
+    address: "東京都渋谷区○○1-2-3",
+    mapUrl: "https://maps.app.goo.gl/CA5XqTvdgpGvoK6a8",
+    maxEntriesAllDay: 0, // 全日最大人数
+    maxEntriesMorning: 0, // 午前最大人数
+    maxEntriesAfternoon: 0, // 午後最大人数
+  },
+  {
+    id: 2,
+    date: "2025年6月15日",
+    store: "サンプルカフェ新宿店",
+    address: "東京都新宿区△△4-5-6",
+    mapUrl: "https://maps.app.goo.gl/CA5XqTvdgpGvoK6a8",
+    maxEntriesAllDay: 3,
+    maxEntriesMorning: 2,
+    maxEntriesAfternoon: 2,
+  },
+  {
+    id: 3,
+    date: "2025年6月15日",
+    store: "サンプルカフェ練馬店",
+    address: "東京都練馬区△△4-5-6",
+    mapUrl: "https://maps.app.goo.gl/CA5XqTvdgpGvoK6a8",
+    maxEntriesAllDay: 1,
+    maxEntriesMorning: 1,
+    maxEntriesAfternoon: 0, // 例: 午後は予約不可
+  },
+  {
+    id: 4,
+    date: "2025年6月15日",
+    store: "サンプルカフェ中野店",
+    address: "東京都中野区△△4-5-6",
+    mapUrl: "https://maps.app.goo.gl/CA5XqTvdgpGvoK6a8",
+    maxEntriesAllDay: 5,
+    maxEntriesMorning: 3,
+    maxEntriesAfternoon: 3,
+  },
+  {
+    id: 5,
+    date: "2025年6月15日",
+    store: "サンプルカフェ東京店",
+    address: "東京都千代田区△△4-5-6",
+    mapUrl: "https://maps.app.goo.gl/CA5XqTvdgpGvoK6a8",
+    maxEntriesAllDay: 2,
+    maxEntriesMorning: 1,
+    maxEntriesAfternoon: 1,
+  },
+  {
+    id: 6,
+    date: "2025年6月15日",
+    store: "サンプルカフェ八王子店",
+    address: "東京都八王子市△△4-5-6",
+    mapUrl: "https://maps.app.goo.gl/CA5XqTvdgpGvoK6a8",
+    maxEntriesAllDay: 2,
+    maxEntriesMorning: 1,
+    maxEntriesAfternoon: 1,
+  }
+];
